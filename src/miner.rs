e917b347fe6ef57ad08d6f4c1410eb305f82530d
Rohit <RohitSinghEmail@protonmail.com>
2022-11-13 23:12:04 +0530

    writing sql stuff

--- src/miner.rs
+++ src/miner.rs
@@ -1,7 +1,9 @@
 use {
+    super::schema::miners,
     serde::{Deserialize, Serialize},
+    uuid::Uuid,
+    crate::DBPooledConnection,
 };
-
 
 // ------------------ JSON Payload (Rest)
 #[derive(Debug, Deserialize, Serialize)]
@@ -14,6 +16,17 @@ pub struct Miner {
     pub shares_mined: i32,
 }
 
+impl Miner {
+    pub fn to_miner_dao(&self) -> MinerDAO {
+        MinerDAO {
+            id: Uuid::parse_str(self.id.as_str()).unwrap(),
+            address: Uuid::parse_str(self.address.as_str()).unwrap(),
+            nickname: self.nickname.to_string(),
+            hash_rate: self.hash_rate,
+            shares_mined: self.shares_mined,
+        }
+    }
+}
 
 // ------------------ POST Reques body for new Miner
 #[derive(Debug, Deserialize, Serialize)]
@@ -22,12 +35,46 @@ pub struct NewMinerRequest {
 }
 
 // ------------------ Data Acess Object (DB Table Records)
+#[derive(Queryable, Insertable)]
+#[table_name = "miners"]
 pub struct MinerDAO {
-    pub id: String,
-    pub address: String,
+    pub id: Uuid,
+    pub address: Uuid,
     pub nickname: String,
     pub hash_rate: i32,
     pub shares_mined: i32,
 }
 
+impl MinerDAO {
+    pub fn to_miner(&self, club_name: String) -> Miner {
+        Miner {
+            id: self.id.to_string(),
+            address: self.address.to_string(),
+            club_name,
+            nickname: self.nickname.to_string(),
+            hash_rate: self.hash_rate,
+            shares_mined: self.shares_mined,
+        }
+    }
+}
 
+pub fn get_club_name(_address: Uuid, conn: &DBPooledConnection) -> String {
+    match fetch_wallet_by_id(_address, &conn) {
+        Some(matched_wallet) => matched_wallet.club_name,
+        None => "Club name not found".to_string(),
+    }
+}
+
+pub fn fetch_all_miners(conn: &DBPooledConnection) -> Vec<Miner> {
+    use crate::schema::miners::dsl::*;
+    match miners.load::<MinerDAO>(conn) {
+        Ok(result) => {
+            result.into_iter().map(|m| {
+                let club_name = get_club_name(m.address, conn);
+                m.to_miner(club_name)
+
+            }).collect::<Vec<Miner>>()
+        },
+        Err(_) => vec![],
+    }
+}
--- src/wallet.rs
+++ src/wallet.rs
@@ -1,10 +1,9 @@
-
 use {
-    serde::{Deserialize, Serialize},
-
+    super::schema::wallets,
     crate::miner::*,
+    serde::{Deserialize, Serialize},
+    uuid::Uuid,
 };
-
 
 // ------------------ JSON Payload (Rest)
 #[derive(Debug, Deserialize, Serialize)]
@@ -13,10 +12,18 @@ pub struct Wallet {
     pub club_name: String,
     pub total_hash_rate: i32,
     pub total_shares_mined: i32,
-    pub total_workers_mined: i32,
+    pub total_workers_online: i32,
     pub workers_online: Vec<Miner>,
 }
 
+impl Wallet {
+    pub fn to_wallet_dao(&self) -> WalletDAO {
+        WalletDAO {
+            address: Uuid::parse_str(self.address.as_str()).unwrap(),
+            club_name: self.club_name.to_string(),
+        }
+    }
+}
 
 // ------------------ POST Reques body for new Miner
 #[derive(Debug, Deserialize, Serialize)]
@@ -24,10 +31,23 @@ pub struct NewWalletRequest {
     club_name: String,
 }
 
-// ------------------ Data Acess Object (DB Table Records)
-pub struct MinerDAO {
-    pub address: String,
+// ----------------- Data Acess Object (DB Table Records)
+#[derive(Queryable, Insertable)]
+#[table_name = "wallets"]
+pub struct WalletDAO {
+    pub address: Uuid,
     pub club_name: String,
 }
 
-
+impl WalletDAO {
+    pub fn to_wallet(&self, workers_online: Vec<Miner>) -> Wallet {
+        Wallet {
+            address: self.address.to_string(),
+            club_name: self.club_name.to_string(),
+            total_hash_rate: workers_online.iter().map(|w| w.hash_rate).sum(),
+            total_shares_mined: workers_online.iter().map(|w| w.shares_mined).sum(),
+            total_workers_online: workers_online.len() as i32,
+            workers_online,
+        }
+    }
+}
